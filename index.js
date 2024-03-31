c94910474f255cfbbfa160af26bda73c6e353f2c
BiggerH2 <161705584+BiggerH2@users.noreply.github.com>
2024-04-01 00:49:27 +0300

    array reduce

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// Import the necessary helper functions
+const { setupDOM } = require('./helpers.js');
+
+// Array containing battery batches
+const batteryBatches = [4, 5, 12, 10];
+
+// Calculate the total number of assembled batteries using reduce()
+const totalBatteries = batteryBatches.reduce(function (total, batch) {
+  return total + batch;
+}, 0);
+
+// Test suite using Mocha and Chai
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('reducer', function() {
+  describe('batteries', function() {
+    it('should have a `totalBatteries` variable', function() {
+      expect(totalBatteries).to.exist;
+    });
+
+    it('should have a number as a result', function() {
+      expect(totalBatteries).to.be.a('number');
+    });
+
+    it('should have made the sum of all the assembled batteries', function() {
+      expect(totalBatteries).to.eql(31);
+    });
+  });
+});
--- test/helpers.js
+++ test/helpers.js
@@ -1,20 +1,10 @@
-const chai = require('chai')
-global.expect = chai.expect
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-const babel = require('babel-core');
+// helpers.js
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
+async function loadChai() {
+  const chai = await import('chai');
+  global.expect = chai.expect;
+}
 
-const babelResult = babel.transformFileSync(
-  path.resolve(__dirname, '..', 'index.js'), {
-    presets: ['env']
-  }
-);
-
-const src = babelResult.code
-
-jsdom({
-  html, src
-});
+module.exports = {
+  loadChai,
+};
